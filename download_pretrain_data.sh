1bc5ab620a1005ccb37273acf232dff642ec38c2
Weikai Huang <shen2347@uw.edu>
2025-04-12 03:06:51 +0000

    fix

---
+++ download_pretrain_data.sh
@@ -0,0 +1,16 @@
+set -e  # Exit immediately if any command fails
+
+REMOTE_BUCKET="gs://oe-training-jieyu/weikaih/grounding_dino_data"
+LOCAL_DATA_DIR="/mmdetection/data"
+
+echo "Starting concurrent downloads..."
+
+gsutil -m cp -r "${REMOTE_BUCKET}/coco" "${LOCAL_DATA_DIR}/" &
+gsutil -m cp -r "${REMOTE_BUCKET}/flickr30k_entities" "${LOCAL_DATA_DIR}/" &
+gsutil -m cp -r "${REMOTE_BUCKET}/gqa" "${LOCAL_DATA_DIR}/" &
+gsutil -m cp -r "${REMOTE_BUCKET}/objects365v1" "${LOCAL_DATA_DIR}/" &
+
+# Wait for all background processes to finish
+wait
+
+echo "All downloads completed."
