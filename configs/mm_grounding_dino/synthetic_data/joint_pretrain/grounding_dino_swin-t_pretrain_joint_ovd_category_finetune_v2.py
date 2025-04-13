73da2af831a6aee20cf5854fd90dd5a9859a51b9
Weikai Huang <shen2347@uw.edu>
2025-04-13 19:31:36 +0000

    add

---
+++ configs/mm_grounding_dino/synthetic_data/joint_pretrain/grounding_dino_swin-t_pretrain_joint_ovd_category_finetune_v2.py
@@ -0,0 +1,25 @@
+_base_ = 'grounding_dino_swin-t_pretrain_joint_base.py'
+
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(type='AdamW', lr=0.0002, weight_decay=0.0001),
+    clip_grad=dict(max_norm=0.1, norm_type=2),
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'backbone': dict(lr_mult=0.1),
+            'language_model': dict(lr_mult=0.1),
+        }))
+
+max_epochs = 10
+param_scheduler = [
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=max_epochs,
+        by_epoch=True,
+        milestones=[6],
+        gamma=0.1)
+]
+train_cfg = dict(max_epochs=max_epochs, val_interval=1)
