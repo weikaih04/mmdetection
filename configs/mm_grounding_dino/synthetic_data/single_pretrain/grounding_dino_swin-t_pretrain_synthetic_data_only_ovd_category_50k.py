9ae923b8cdf2b66196b5de0a1de123e8a6d01061
Weikai Huang <shen2347@uw.edu>
2025-04-17 23:53:31 +0000

    fix

---
+++ configs/mm_grounding_dino/synthetic_data/single_pretrain/grounding_dino_swin-t_pretrain_synthetic_data_only_ovd_category_50k.py
@@ -0,0 +1,66 @@
+_base_ = 'grounding_dino_swin-t_pretrain_synthetic_data_only_base.py'
+
+ovd_category_train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(
+        type='RandomChoice',
+        transforms=[
+            [
+                dict(
+                    type='RandomChoiceResize',
+                    scales=[(480, 1333), (512, 1333), (544, 1333), (576, 1333),
+                            (608, 1333), (640, 1333), (672, 1333), (704, 1333),
+                            (736, 1333), (768, 1333), (800, 1333)],
+                    keep_ratio=True)
+            ],
+            [
+                dict(
+                    type='RandomChoiceResize',
+                    # The radio of all image in train dataset < 7
+                    # follow the original implement
+                    scales=[(400, 4200), (500, 4200), (600, 4200)],
+                    keep_ratio=True),
+                dict(
+                    type='RandomCrop',
+                    crop_type='absolute_range',
+                    crop_size=(384, 600),
+                    allow_negative_crop=True),
+                dict(
+                    type='RandomChoiceResize',
+                    scales=[(480, 1333), (512, 1333), (544, 1333), (576, 1333),
+                            (608, 1333), (640, 1333), (672, 1333), (704, 1333),
+                            (736, 1333), (768, 1333), (800, 1333)],
+                    keep_ratio=True)
+            ]
+        ]),
+    dict(type='FilterAnnotations', min_gt_bbox_wh=(1e-2, 1e-2)),
+    dict(
+        type='RandomSamplingNegPos',
+        tokenizer_name=_base_.lang_model_name,
+        num_sample_negative=85,
+        # change this
+        label_map_file='data/ovd_50k/annotations/label_map.json',
+        max_tokens=256),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor', 'flip', 'flip_direction', 'text',
+                   'custom_entities', 'tokens_positive', 'dataset_mode'))
+]
+
+ovd_category_dataset = dict(
+    type='ODVGDataset',
+    data_root='data/ovd/',
+    ann_file='annotations/panoptic_train_50000_odvg_category.json',
+    label_map_file='annotations/panoptic_train_50000_label_map.json',
+    data_prefix=dict(img='train/'),
+    filter_cfg=dict(filter_empty_gt=True),
+    pipeline=ovd_category_train_pipeline,
+    return_classes=True,
+    backend_args=None,
+)
+
+train_dataloader = dict(
+    dataset=dict(datasets=[ovd_category_dataset]))
--- download_pretrain_data.sh
+++ download_pretrain_data.sh
@@ -5,10 +5,11 @@ LOCAL_DATA_DIR="/mmdetection/data"
 
 echo "Starting concurrent downloads..."
 
-gsutil -m cp -r "${REMOTE_BUCKET}/coco" "${LOCAL_DATA_DIR}/" &
+gsutil -m cp -r "${REMOTE_BUCKET}/coco" "${LOCAL_DATA_DIR}/" &z
 gsutil -m cp -r "${REMOTE_BUCKET}/flickr30k_entities" "${LOCAL_DATA_DIR}/" &
 gsutil -m cp -r "${REMOTE_BUCKET}/gqa" "${LOCAL_DATA_DIR}/" &
 gsutil -m cp -r "${REMOTE_BUCKET}/objects365v1" "${LOCAL_DATA_DIR}/" &
+gsutil -m cp -r "${REMOTE_BUCKET}/odinw" "${LOCAL_DATA_DIR}/" &
 
 # Wait for all background processes to finish
 wait
