ac864b1fac46016fadab9058112af22821640042
Weikai Huang <shen2347@uw.edu>
2025-04-17 23:17:42 +0000

    fix

--- download_1m_data.sh
+++ download_1m_data.sh
@@ -2,25 +2,23 @@
 set -e
 
 REMOTE_BUCKET="gs://oe-training-jieyu/weikaih/grounding_dino_synthetic_data/ovd_1m"
-LOCAL_ROOT="/mmdetection/data/ovd_1m"
+LOCAL_ROOT="/mmdetection/data/ovd"
 
 echo "Preparing local directory…"
 mkdir -p \
-  "${LOCAL_ROOT}/panoptic_train/annotations" \
-  "${LOCAL_ROOT}/annotation" \
+  "${LOCAL_ROOT}/panoptic_train" \
+  "${LOCAL_ROOT}/annotations" \
   "${LOCAL_ROOT}/train"
 
 echo "Starting selective downloads…"
 
 # 1) panoptic_train annotations only
 gsutil -m cp -r \
-  "${REMOTE_BUCKET}/panoptic_train/annotations" \
-  "${LOCAL_ROOT}/panoptic_train/annotations" &
+  "${REMOTE_BUCKET}/panoptic_train/*" \
+  "${LOCAL_ROOT}/panoptic_train" &
 
 # 2) top‑level annotation folder (singular)
-gsutil -m cp -r \
-  "${REMOTE_BUCKET}/annotation" \
-  "${LOCAL_ROOT}/annotation" &
+gsutil -m rsync -r -x "separate_annotations/" ${REMOTE_BUCKET}/annotations ${LOCAL_ROOT}/annotations &
 
 # 3) train_keep_color → renamed locally to 'train'
 gsutil -m cp -r \
