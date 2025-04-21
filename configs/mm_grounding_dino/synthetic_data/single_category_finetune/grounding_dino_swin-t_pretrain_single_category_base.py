a1b6ba7b1275ddfded4033be15f285dbd684039c
Weikai Huang <shen2347@uw.edu>
2025-04-21 20:31:03 +0000

    add new exp

---
+++ configs/mm_grounding_dino/synthetic_data/single_category_finetune/grounding_dino_swin-t_pretrain_single_category_base.py
@@ -0,0 +1,70 @@
+_base_ = '../grounding_dino_swin-t_pretrain_obj365.py'
+
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(type='AdamW', lr=0.0004,
+                   weight_decay=0.0001),
+    clip_grad=dict(max_norm=0.1, norm_type=2),
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'backbone': dict(lr_mult=0.1),
+            'language_model': dict(lr_mult=0.1),
+        }
+    ),
+)
+
+model = dict(
+    test_cfg=dict(max_per_img=30)
+)
+
+data_root = 'data/coco/'
+
+# pipeline for testing
+test_pipeline = [
+    dict(
+        type='LoadImageFromFile', backend_args=None,
+        imdecode_backend='pillow'),
+    dict(
+        type='FixScaleResize',
+        scale=(800, 1333),
+        keep_ratio=True,
+        backend='pillow'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=(
+            'img_id', 'img_path', 'ori_shape', 'img_shape',
+            'scale_factor', 'text', 'custom_entities',
+            'tokens_positive'
+        )
+    )
+]
+
+# single-category dataset & evaluator
+val_dataset_single_cat = dict(
+    type='SingleCategoryRefDataset',
+    data_root=data_root,
+    ann_file='mdetr_annotations/single_category_ref_4_19.json',
+    data_prefix=dict(img='single_category_ref/'),
+    test_mode=True,
+    return_classes=True,
+    filter_cfg=dict(filter_empty_gt=False, cat_ids=[1]),
+    pipeline=test_pipeline,
+)
+
+val_evaluator_single_cat = dict(
+    type='SingleCategoryRefMetric',
+    ann_file=data_root + 'mdetr_annotations/single_category_ref_4_19.json',
+    iou_thrs=0.5
+)
+
+# override default dataloader & evaluator
+val_dataloader = dict(
+    dataset=dict(_delete_=True, **val_dataset_single_cat)
+)
+test_dataloader = val_dataloader
+
+val_evaluator = val_evaluator_single_cat
+test_evaluator = val_evaluator
---
+++ download_200k_ref_single_category_data.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+REMOTE_BUCKET="gs://oe-training-jieyu/weikaih/grounding_dino_synthetic_data/ref_200k_single_category"
+LOCAL_ROOT="/mmdetection/data/ref_single_category"
+
+echo "Preparing local directory…"
+mkdir -p \
+  "${LOCAL_ROOT}/panoptic_train" \
+  "${LOCAL_ROOT}/annotations" \
+  "${LOCAL_ROOT}/train"
+
+echo "Starting selective downloads…"
+
+# 1) panoptic_train annotations only
+gsutil -m cp -r \
+  "${REMOTE_BUCKET}/panoptic_train/*" \
+  "${LOCAL_ROOT}/panoptic_train" &
+
+# 2) top‑level annotation folder (singular)
+gsutil -m rsync -r -x "separate_annotations/" ${REMOTE_BUCKET}/annotations ${LOCAL_ROOT}/annotations &
+
+# 3) train_keep_color → renamed locally to 'train'
+gsutil -m cp -r \
+  "${REMOTE_BUCKET}/train_keep_color_v2/*" \
+  "${LOCAL_ROOT}/train" &
+
+wait
+
+echo "All done!"
