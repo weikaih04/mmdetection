3ceae91fcab36329c7ec55a0018078f520b65c08
Weikai Huang <shen2347@uw.edu>
2025-04-27 19:59:20 +0000

    fix

--- download_eval_data.sh
+++ download_eval_data.sh
@@ -5,12 +5,12 @@ LOCAL_DATA_DIR="/mmdetection/data"
 
 echo "Starting concurrent downloads..."
 
-# gsutil -m cp -r "${REMOTE_BUCKET}/coco" "${LOCAL_DATA_DIR}/" &
-# gsutil -m cp -r "${REMOTE_BUCKET}/flickr30k_entities" "${LOCAL_DATA_DIR}/" &
-# gsutil -m cp -r "${REMOTE_BUCKET}/gqa" "${LOCAL_DATA_DIR}/" &
-# gsutil -m cp -r "${REMOTE_BUCKET}/objects365v1" "${LOCAL_DATA_DIR}/" &
-# gsutil -m cp -r "${REMOTE_BUCKET}/odinw" "${LOCAL_DATA_DIR}/" &
-# gsutil -m cp -r "${REMOTE_BUCKET}/d3" "${LOCAL_DATA_DIR}/" &
+gsutil -m cp -r "${REMOTE_BUCKET}/coco" "${LOCAL_DATA_DIR}/" &
+gsutil -m cp -r "${REMOTE_BUCKET}/flickr30k_entities" "${LOCAL_DATA_DIR}/" &
+gsutil -m cp -r "${REMOTE_BUCKET}/gqa" "${LOCAL_DATA_DIR}/" &
+gsutil -m cp -r "${REMOTE_BUCKET}/objects365v1" "${LOCAL_DATA_DIR}/" &
+gsutil -m cp -r "${REMOTE_BUCKET}/odinw" "${LOCAL_DATA_DIR}/" &
+gsutil -m cp -r "${REMOTE_BUCKET}/d3" "${LOCAL_DATA_DIR}/" &
 gsutil -m cp -r "${REMOTE_BUCKET}/v3det" "${LOCAL_DATA_DIR}/" &
 
 # Wait for all background processes to finish
